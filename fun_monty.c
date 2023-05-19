d81994e94af5dfbff64712268f7c8ac392da37bb
HaroldS10 <haroldsanty13@gmail.com>
2023-05-19 12:54:06 -0700

    betty corrections

--- fun_monty.c
+++ fun_monty.c
@@ -37,15 +37,15 @@ void push(stack_t **stack, unsigned int line_num, int n)
 
 void pall(stack_t **stack, unsigned int line_num)
 {
-        stack_t *h = *stack;
+	stack_t *h = *stack;
 
-        (void)line_num;
+	(void)line_num;
 
-        while (h)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-        }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
 
 /**
