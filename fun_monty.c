8e48d792395551ef495cfe3a61ad1cf3a0a0c244
HaroldS10 <haroldsanty13@gmail.com>
2023-05-19 12:46:40 -0700

    betty corrections

--- fun_monty.c
+++ fun_monty.c
@@ -11,20 +11,21 @@
 void push(stack_t **stack, unsigned int line_num, int n)
 {
 	stack_t *new, *h = *stack;
-        if (stack == NULL)
-        {
-                fprintf(stderr, "L%d: usage: push integer\n", line_num);
-                exit(EXIT_FAILURE);
-        }
-        new = malloc(sizeof(stack_t));
-        if (new == NULL)
-                exit(EXIT_FAILURE);
-        new->prev = NULL;
-        new->n = n;
-        new->next = *stack;
-        if (*stack)
-                h->prev = new;
-        *stack = new;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		exit(EXIT_FAILURE);
+	new->prev = NULL;
+	new->n = n;
+	new->next = *stack;
+	if (*stack)
+		h->prev = new;
+	*stack = new;
 }
 
 /**
@@ -36,14 +37,14 @@ void push(stack_t **stack, unsigned int line_num, int n)
 
 void pall(stack_t **stack, unsigned int line_num)
 {
-	stack_t *h = *stack;
-	(void)line_num;
+        stack_t *h = *stack;
+        (void)line_num;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
 }
 
 /**
@@ -56,11 +57,14 @@ void pall(stack_t **stack, unsigned int line_num)
 void pop(stack_t **stack, unsigned int line_num)
 {
 	stack_t *h = *stack;
+
 	if (!(*stack))
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
 		exit(EXIT_FAILURE);
 	}
+
+
 	if (h)
 	{
 		*stack = (h)->next;
@@ -74,15 +78,16 @@ void pop(stack_t **stack, unsigned int line_num)
  * @line_num: file's line number
  * Return:  Void
  */
-
 void swap(stack_t **stack, unsigned int line_num)
 {
 	stack_t *h = *stack, *ptr;
+
 	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
+
 	if (h && h->next)
 	{
 		ptr = h->next;
